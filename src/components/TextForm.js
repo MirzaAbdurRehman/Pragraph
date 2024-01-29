9d11d21c722674fc60e2a1142f2323ed6ce93061
Mirza Abdur Rehman <126644528+MirzaAbdurRehman@users.noreply.github.com>
2024-01-29 12:50:10 +0500

    new
    new

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -76,11 +76,11 @@ export default function TextForm(props) {
                 <div className= "container my-4" style={{color: props.mode==='dark'?'white':'#042743'}} >
                     <h1>{props.heading}</h1>
                     <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" value={text} onChange={handleOnChange} rows="6"></textarea><br />
-                    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                    <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
-                    <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear The Text</button>
-                    <button className="btn btn-primary mx-1" onClick={handleExtraSpacesClick}>Remove Extra Spaces</button>
-                    <button className="btn btn-primary mx-1" onClick={handleTextCopyClick}>Copy The Text</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear The Text</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpacesClick}>Remove Extra Spaces</button>
+                    <button className="btn btn-primary mx-1 my-1" onClick={handleTextCopyClick}>Copy The Text</button>
                     {/* <button  onClick={toggleStyle} className='btn btn-primary'>{btntext}</button> */}
                 </div>
                 <div className="conatiner my-3 mx-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
